e0538592e6300b629e5bc174d4617101f22f322c
Oleh <olvolo17@gmail.com>
2024-06-21 20:10:11 +0300

    Create AccountNotFoundException

---
+++ src/main/java/org/example/spring_demo/exeption/AccountNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.spring_demo.exeption;
+
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
