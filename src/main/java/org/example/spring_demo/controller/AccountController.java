3d1e847a2eff16db825a71c0d869f491a314ae4a
Oleh <olvolo17@gmail.com>
2024-06-21 16:27:58 +0300

    Add an annotation @RestController and dependency

--- src/main/java/org/example/spring_demo/controller/AccountController.java
+++ src/main/java/org/example/spring_demo/controller/AccountController.java
@@ -1,5 +1,8 @@
 package org.example.spring_demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
     public String helloSpring(){
         return "Hello Spring!";
